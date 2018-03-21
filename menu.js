184fcf61d6d20e1b8efa871f310cf8ffd47a8277
Weixuhong <weixuhong@vtstar.net>
2018-03-21 23:32:52 +0800

    update menu

--- menu.js
+++ menu.js
@@ -10,9 +10,10 @@ let template = [
         submenu: [
             {
                 label:'JENKINS',
+                accelerator: 'Shift+CmdOrCtrl+J',
                 click: function(){
                     let mainWindow = new BrowserWindow({
-                        title: '文什项目管理',
+                        title: 'JENKINS',
                         width: 800,
                         height: 600,
                         webPreferences: {
@@ -23,6 +24,24 @@ let template = [
                         }
                     });
                     mainWindow.loadURL('http://fap.wenshidata.com:9999') 
+                }
+            },
+            {
+                label:'CODE',
+                accelerator: 'Shift+CmdOrCtrl+C',
+                click: function(){
+                    let mainWindow = new BrowserWindow({
+                        title: 'CODE',
+                        width: 800,
+                        height: 600,
+                        webPreferences: {
+                            // allowRunningInsecureContent: true,
+                            // webSecurity: false,
+                            preload: path.resolve('./preload.js'),
+                            nodeIntegration: false //doesn't matter if node integration turned off or on, same result
+                        }
+                    });
+                    mainWindow.loadURL('https://code.wenshidata.com') 
                 }
             }
         ]
@@ -74,24 +93,28 @@ let template = [
         submenu: [
             {
                 label:'富朗特',
+                accelerator: 'CmdOrCtrl+1',
                 click: function(){
                     BrowserWindow.getFocusedWindow().loadURL('http://fap.wenshidata.com/projects/fap/issues?query_id=6') 
                 }
             },
             {
                 label:'SCT',
+                accelerator: 'CmdOrCtrl+2',
                 click: function(){
                     BrowserWindow.getFocusedWindow().loadURL('http://fap.wenshidata.com/projects/f-sct/issues?query_id=6') 
                 }
             },
             {
                 label:'FAP',
+                accelerator: 'CmdOrCtrl+3',
                 click: function(){
                     BrowserWindow.getFocusedWindow().loadURL('http://fap.wenshidata.com/projects/faq/issues?query_id=6') 
                 }
             },
             {
                 label:'移动端',
+                accelerator: 'CmdOrCtrl+4',
                 click: function(){
                     BrowserWindow.getFocusedWindow().loadURL('http://fap.wenshidata.com/projects/far/issues?query_id=6') 
                 }
@@ -103,7 +126,7 @@ let template = [
         submenu: [
             {
                 label: '项目',
-                accelerator: 'CmdOrCtrl+H',
+                accelerator: 'Shift+CmdOrCtrl+H',
                 click: function () {
                     BrowserWindow.getFocusedWindow().loadURL('http://fap.wenshidata.com/projects')
                 }
