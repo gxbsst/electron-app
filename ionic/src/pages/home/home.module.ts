70af4eee5b7093a55502091a576078b322b6f142
Weixuhong <weixuhong@vtstar.net>
2018-03-29 11:47:19 +0800

    修复js报错的部分

--- ionic/src/pages/home/home.module.ts
+++ ionic/src/pages/home/home.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {IonicModule} from 'ionic-angular';
 import {HomePage} from './home';
 import {TabModule} from '../tabs/tab.module'
+import {NgZorroAntdModule} from "ng-zorro-antd";
 
 @NgModule({
-  imports: [IonicModule],
+  imports: [IonicModule,NgZorroAntdModule],
   declarations: [HomePage],
   entryComponents: [HomePage],
   providers: [],
