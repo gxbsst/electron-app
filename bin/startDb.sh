3c9bd6bc19fb888df116080d0632d2836e718120
Weixuhong <weixuhong@vtstar.net>
2018-04-16 19:43:48 +0800

    update

--- bin/startDb.sh
+++ bin/startDb.sh
@@ -2,4 +2,4 @@
 
 PWD=$(pwd)
 
-java -classpath ${PWD}/cuba/lib/hsqldb-2.2.9.jar  org.hsqldb.Server -database.0 ${PWD}/cuba/db/cuba -dbname.0 cuba
+java -classpath ${PWD}/cuba/lib/hsqldb-2.2.9.jar  org.hsqldb.Server -database.0 ${PWD}/cuba/db/cuba -dbname.0 cuba -database.1 ${PWD}/cuba/db/wsprintermanager -dbname.1 wsprintermanager
